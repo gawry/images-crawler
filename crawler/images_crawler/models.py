e31214e27ff04d66cfc79665308ee0299e1892de
Gustavo Gawryszewski <318831+gawry@users.noreply.github.com>
2024-01-31 20:03:03 -0300

    updated sqlalchemy call

--- crawler/images_crawler/models.py
+++ crawler/images_crawler/models.py
@@ -1,6 +1,5 @@
-from sqlalchemy import create_engine, Column, Integer, String, DateTime
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.orm import declarative_base
 import datetime
 
 Base = declarative_base()
